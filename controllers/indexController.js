23179d0503fd5db99a38ef81d12e950d4741b0c5
Benja Aguilar <benjaaguilar02@gmail.com>
2024-11-11 18:31:48 -0300

    feat: add auth route protection

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,19 +1,40 @@
 async function getIndex(req, res, next){
+    if(res.locals.isAuth){
+        return res.redirect('/mystorage');
+    }
+    
     res.render('index');
 }
 
 async function getSignUp(req, res, next){
+    if(res.locals.isAuth){
+        return res.redirect('/mystorage');
+    }
+
     res.render('signup');
 }
 
 async function getLogIn(req, res, next){
+    if(res.locals.isAuth){
+        return res.redirect('/mystorage');
+    }
+
     res.render('login');
+}
+
+async function getMyStorage(req, res, next) {
+    if(!res.locals.isAuth){
+        return res.redirect('/');
+    }
+
+    res.render('mystorage');
 }
 
 const indexController = {
     getIndex,
     getSignUp,
-    getLogIn
+    getLogIn,
+    getMyStorage
 };
 
 export default indexController;
