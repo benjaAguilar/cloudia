aeba3e946010e4fa76ac71d2a944dc7fe337e416
Benja Aguilar <benjaaguilar02@gmail.com>
2024-11-11 18:34:03 -0300

    feat: add routes

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import indexController from '../controllers/indexController.js';
+import userController from '../controllers/userController.js';
 const router = Router();
 
 router.get('/', indexController.getIndex);
@@ -7,5 +8,9 @@ router.get('/', indexController.getIndex);
 router.get('/signup', indexController.getSignUp);
 
 router.get('/login', indexController.getLogIn);
+
+router.get('/mystorage', indexController.getMyStorage);
+
+router.post('/signup', userController.postCreateUser);
 
 export default router;
