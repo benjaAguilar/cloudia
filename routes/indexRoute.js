b099e324aac2116a2178b639478db6d5e755c365
Benja Aguilar <benjaaguilar02@gmail.com>
2024-11-12 11:08:48 -0300

    feat: add error handling and post login route

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,16 +1,19 @@
 import { Router } from 'express';
 import indexController from '../controllers/indexController.js';
 import userController from '../controllers/userController.js';
+import tryCatch from '../utils/tryCatch.js';
 const router = Router();
 
-router.get('/', indexController.getIndex);
+router.get('/', tryCatch(indexController.getIndex));
 
-router.get('/signup', indexController.getSignUp);
+router.get('/signup', tryCatch(indexController.getSignUp));
 
-router.get('/login', indexController.getLogIn);
+router.get('/login', tryCatch(indexController.getLogIn));
 
-router.get('/mystorage', indexController.getMyStorage);
+router.get('/mystorage', tryCatch(indexController.getMyStorage));
 
 router.post('/signup', userController.postCreateUser);
+
+router.post('/login', tryCatch(userController.postLogUser));
 
 export default router;
